64c465adbf5d8b0b5847aa13bc9b670886894986
Davi Pereira <davi.p.m94@gmail.com>
2025-08-21 15:12:13 +0900

    Extract `CategoryCard` component for modularity and refactor `CategoryCards` to utilize it
    - Introduced a new `CategoryCard` component to encapsulate individual category card logic and actions (`Edit` and `Delete`).
    - Updated `CategoryCards` to use `CategoryCard`, simplifying its structure and enhancing reusability.
    - Improved code readability by moving action handlers and UI logic into `CategoryCard`.

--- app/(dashboard)/admin/foods-management/categories/_components/category-cards.tsx
+++ app/(dashboard)/admin/foods-management/categories/_components/category-cards.tsx
@@ -1,65 +1,37 @@
 'use client';
 
+import { CategoryCard } from '@/app/(dashboard)/admin/foods-management/categories/_components/category-card';
 import { CategoryCardsSkeleton } from '@/app/(dashboard)/admin/foods-management/categories/_components/category-cards-skeleton';
 import { useCategoriesStore } from '@/app/(dashboard)/admin/foods-management/categories/_libs/use-categories-store';
-import { useDeleteCategory } from '@/app/(dashboard)/admin/foods-management/categories/_services/use-mutations';
 import { useCategories } from '@/app/(dashboard)/admin/foods-management/categories/_services/use-queries';
 import NoItemFound from '@/components/no-item-found';
-import { Button } from '@/components/ui/button';
-import { alert } from '@/store/use-global-store';
-import { Edit, Trash } from 'lucide-react';
 
 export function CategoryCards() {
-  const { updateSelectedCategoryId, updateCategoryDialogOpen } = useCategoriesStore();
+  const { updateCategoryDialogOpen } = useCategoriesStore();
 
-  const categoryQuery = useCategories();
-  const deleteCategoryMutation = useDeleteCategory();
+  const { data, isLoading, isError } = useCategories();
 
-  if (categoryQuery.data?.length === 0) {
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-4">
+        <CategoryCardsSkeleton />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <p className="text-center text-red-500">Failed to load categories.</p>;
+  }
+
+  if (data?.length === 0) {
     return <NoItemFound onClick={() => updateCategoryDialogOpen(true)} />;
   }
 
   return (
     <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-4">
-      {categoryQuery.isLoading ? (
-        <CategoryCardsSkeleton />
-      ) : (
-        <>
-          {categoryQuery.data?.map((item) => (
-            <div
-              key={item.id}
-              className="flex flex-col justify-between gap-3 rounded-lg border p-6"
-            >
-              <p className="truncate">{item.name}</p>
-              <div className="flex gap-1">
-                <Button
-                  className="size-6"
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => {
-                    updateSelectedCategoryId(item.id);
-                    updateCategoryDialogOpen(true);
-                  }}
-                >
-                  <Edit />
-                </Button>
-                <Button
-                  className="size-6"
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => {
-                    alert({
-                      onConfirm: () => deleteCategoryMutation.mutate(item.id),
-                    });
-                  }}
-                >
-                  <Trash />
-                </Button>
-              </div>
-            </div>
-          ))}
-        </>
-      )}
+      {data?.map((category) => (
+        <CategoryCard key={category.id} category={category} />
+      ))}
     </div>
   );
 }
